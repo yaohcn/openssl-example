a0984270bbada40bd57b3084c0afc87bd8bb9c08
yaohcn <yaohua_yh@126.com>
2018-07-24 23:59:46 +0800

    add comment

--- base64_encode_exp.c
+++ base64_encode_exp.c
@@ -1,42 +1,47 @@
+/**
+ * https://zh.wikipedia.org/wiki/Base64
+ * base64是一种基于64个可打印字符来表示任意二进制数据的方法
+ * 编码后的长度是原来的4/3（二进制转换成字符串长度是原来的2倍）
+ * 便于传输，能够简单加密
+ **/
 #include <string.h>
-
 #include <openssl/sha.h>
 #include <openssl/evp.h>
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
 
-
 char *base64(const unsigned char *input, int length)
 {
-  BIO *bmem, *b64;
-  BUF_MEM *bptr;
+    BIO *bmem, *b64;
+    BUF_MEM *bptr;
 
-  b64 = BIO_new(BIO_f_base64());
-  bmem = BIO_new(BIO_s_mem());
-  //不要加入换行符
-  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-  b64 = BIO_push(b64, bmem);
-  BIO_write(b64, input, length);
-  BIO_flush(b64);
-  BIO_get_mem_ptr(b64, &bptr);
+    b64 = BIO_new(BIO_f_base64());
+    bmem = BIO_new(BIO_s_mem());
+    //在电子邮件中，根据RFC 822规定，每76个字符，还需要加上一个回车换行。
+    //不要加入换行符
+    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+    b64 = BIO_push(b64, bmem);
+    BIO_write(b64, input, length);
+    BIO_flush(b64);
+    BIO_get_mem_ptr(b64, &bptr);
 
-  char *buff = (char *)malloc(bptr->length);
-  memcpy(buff, bptr->data, bptr->length-1);
-  buff[bptr->length-1] = 0;
+    char *buff = (char *)malloc(bptr->length);
+    memcpy(buff, bptr->data, bptr->length-1);
+    buff[bptr->length-1] = 0;
 
-  BIO_free_all(b64);
+    BIO_free_all(b64);
 
-  return buff;
+    return buff;
 }
 
 
 int main()
 {
-  const unsigned char clearText[] = "disu isu supaaataa";
+    const unsigned char clearText[] = "disu isu supaaataa";
 
-  char *output = base64((unsigned char*)clearText, sizeof(clearText));
-  printf("Base64: '%s'\n", output);
-  free(output);
+    char *output = base64((unsigned char*)clearText, sizeof(clearText));
+    printf("Base64: '%s'\n", output);
+    free(output);
 
-  return 0;
+    return 0;
 }
--- rsa_sign_exp.c
+++ rsa_sign_exp.c
@@ -1,5 +1,21 @@
 /*  gcc ./openssl_sign.c -lssl */
-
+/*
+ * asn.1是一种描述数据结构的标准，ASN.1本身只定义了表示信息的抽象句法，但是没有限定其编码的方法。
+ * 各种ASN.1编码规则提供了由ASN.1描述其抽象句法的数据的值的传送语法（具体表达）。
+ * 标准的ASN.1编码规则有ber，cer，der等。
+ * 简单的说，asn.1是标准，der是asn.1的一种实现。
+ * DER是二进制，不便于显示和传输，于是有了PEM。PEM仅仅是对DER进行base64编码。
+ * DER is a binary format for data structures described by ASN.1.
+ *
+ * PKCS公开密钥加密标准
+ * https://zh.wikipedia.org/wiki/%E5%85%AC%E9%92%A5%E5%AF%86%E7%A0%81%E5%AD%A6%E6%A0%87%E5%87%86
+ *
+ * OpenSSL 工具使用
+ * OpenSSL> genrsa -out app_private_key.pem   2048  #生成私钥
+ * OpenSSL> pkcs8 -topk8 -inform PEM -in app_private_key.pem -outform PEM -nocrypt -out app_private_key_pkcs8.pem #Java开发者需要将私钥转换成PKCS8格式
+ * OpenSSL> rsa -in app_private_key.pem -pubout -out app_public_key.pem #生成公钥
+ * OpenSSL> exit #退出OpenSSL程序
+ * */
 #include <stdio.h>
 #include <string.h>
 #include <error.h>
@@ -12,111 +28,111 @@
 
 int pass_cb( char *buf, int size, int rwflag, void *u )
 {
-  if ( rwflag == 1 ) {
-    /* What does this really means? */
-  }
+    if ( rwflag == 1 ) {
+        /* What does this really means? */
+    }
 
-  int len;
-  char tmp[1024];
-  printf( "Enter pass phrase for '%s': ", (char*)u );
-  scanf( "%s", tmp );
-  len = strlen( tmp );
+    int len;
+    char tmp[1024];
+    printf( "Enter pass phrase for '%s': ", (char*)u );
+    scanf( "%s", tmp );
+    len = strlen( tmp );
 
-  if ( len <= 0 ) return 0;
-  if ( len > size ) len = size;
+    if ( len <= 0 ) return 0;
+    if ( len > size ) len = size;
 
-  memset( buf, '\0', size );
-  memcpy( buf, tmp, len );
-  return len;
+    memset( buf, '\0', size );
+    memcpy( buf, tmp, len );
+    return len;
 }
 
 RSA* getRsaFp( const char* rsaprivKeyPath )
 {
-  FILE* fp;
-  fp = fopen( rsaprivKeyPath, "r" );
-  if ( fp == 0 ) {
-    fprintf( stderr, "Couldn't open RSA priv key: '%s'. %s\n",
-             rsaprivKeyPath, strerror(errno) );
-    exit(1);
-  }
+    FILE* fp;
+    fp = fopen( rsaprivKeyPath, "r" );
+    if ( fp == 0 ) {
+        fprintf( stderr, "Couldn't open RSA priv key: '%s'. %s\n",
+                rsaprivKeyPath, strerror(errno) );
+        exit(1);
+    }
 
-  RSA *rsa = 0;
-  rsa = RSA_new();
-  if ( rsa == 0 ) {
-    fprintf( stderr, "Couldn't create new RSA priv key obj.\n" );
-    unsigned long sslErr = ERR_get_error();
-    if ( sslErr ) fprintf(stderr, "%s\n", ERR_error_string(sslErr, 0));
-    fclose( fp );
-    exit( 1 );
-  }
+    RSA *rsa = 0;
+    rsa = RSA_new();
+    if ( rsa == 0 ) {
+        fprintf( stderr, "Couldn't create new RSA priv key obj.\n" );
+        unsigned long sslErr = ERR_get_error();
+        if ( sslErr ) fprintf(stderr, "%s\n", ERR_error_string(sslErr, 0));
+        fclose( fp );
+        exit( 1 );
+    }
 
-  rsa = PEM_read_RSAPrivateKey(fp, 0, pass_cb, (char*)rsaprivKeyPath);
-  if ( rsa == 0 ) {
-    fprintf( stderr, "Couldn't use RSA priv keyfile.\n" );
-    unsigned long sslErr = ERR_get_error();
-    if ( sslErr ) fprintf(stderr, "%s\n", ERR_error_string(sslErr, 0));
+    rsa = PEM_read_RSAPrivateKey(fp, 0, pass_cb, (char*)rsaprivKeyPath);
+    if ( rsa == 0 ) {
+        fprintf( stderr, "Couldn't use RSA priv keyfile.\n" );
+        unsigned long sslErr = ERR_get_error();
+        if ( sslErr ) fprintf(stderr, "%s\n", ERR_error_string(sslErr, 0));
+        fclose( fp );
+        exit( 1 );
+    }
     fclose( fp );
-    exit( 1 );
-  }
-  fclose( fp );
-  return rsa;
+    return rsa;
 }
 
 
 int main( int argc, char* argv[] )
 {
-  if ( argc != 2 ) {
-    fprintf( stderr, "Usage: %s <text to sign>\n", argv[0] );
-    exit( 1 );
-  }
-  const char *clearText = argv[1];
+    if ( argc != 2 ) {
+        fprintf( stderr, "Usage: %s <text to sign>\n", argv[0] );
+        exit( 1 );
+    }
+    const char *clearText = argv[1];
 
-  char rsaprivKeyPath[1024];
-  sprintf( rsaprivKeyPath, "%s/.ssh/id_rsa",  getenv ("HOME") );
+    char rsaprivKeyPath[1024];
+    sprintf( rsaprivKeyPath, "%s/.ssh/id_rsa",  getenv ("HOME") );
 
-  SSL_load_error_strings();
+    SSL_load_error_strings();
 
-  OpenSSL_add_all_algorithms();
-  OpenSSL_add_all_ciphers();
-  OpenSSL_add_all_digests();
+    OpenSSL_add_all_algorithms();
+    OpenSSL_add_all_ciphers();
+    OpenSSL_add_all_digests();
 
-  RSA *rsa = 0;
-  rsa = getRsaFp( rsaprivKeyPath );
+    RSA *rsa = 0;
+    rsa = getRsaFp( rsaprivKeyPath );
 
-  SHA256_CTX context;
-  unsigned char msgDigest[SHA256_DIGEST_LENGTH];  // 32
+    SHA256_CTX context;
+    unsigned char msgDigest[SHA256_DIGEST_LENGTH];  // 32
 
-  SHA256_Init(&context);
-  SHA256_Update(&context, (unsigned char*)clearText, strlen(clearText));
-  SHA256_Final(msgDigest, &context);
+    SHA256_Init(&context);
+    SHA256_Update(&context, (unsigned char*)clearText, strlen(clearText));
+    SHA256_Final(msgDigest, &context);
 
-  int i;
-  printf("Digest: ");
-  for (i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-    printf("%02x", msgDigest[i]);
-  }
-  printf("\n");
+    int i;
+    printf("Digest: ");
+    for (i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        printf("%02x", msgDigest[i]);
+    }
+    printf("\n");
 
 
-  const int MAX_LEN = RSA_size(rsa);  //256
-  unsigned char sigRet[MAX_LEN];
-  unsigned int sigLen = 0;
-  memset(sigRet, 0, MAX_LEN);
+    const int MAX_LEN = RSA_size(rsa);  //256
+    unsigned char sigRet[MAX_LEN];
+    unsigned int sigLen = 0;
+    memset(sigRet, 0, MAX_LEN);
 
-  if ( RSA_sign(NID_sha1, msgDigest, SHA256_DIGEST_LENGTH, sigRet, &sigLen, rsa) == 0 ) {
-    fprintf( stderr, "Couldn't sign message digest.\n" );
-    unsigned long sslErr = ERR_get_error();
-    if ( sslErr ) fprintf(stderr, "%s\n", ERR_error_string(sslErr, 0));
-    exit( 1 );
-  }
+    if ( RSA_sign(NID_sha1, msgDigest, SHA256_DIGEST_LENGTH, sigRet, &sigLen, rsa) == 0 ) {
+        fprintf( stderr, "Couldn't sign message digest.\n" );
+        unsigned long sslErr = ERR_get_error();
+        if ( sslErr ) fprintf(stderr, "%s\n", ERR_error_string(sslErr, 0));
+        exit( 1 );
+    }
 
-  printf("Signature: ");
-  for (i = 0; i < MAX_LEN; i++) {
-    printf("%02x", sigRet[i]);
-  }
-  printf("\n");
+    printf("Signature: ");
+    for (i = 0; i < MAX_LEN; i++) {
+        printf("%02x", sigRet[i]);
+    }
+    printf("\n");
 
-  RSA_free( rsa );
-  ERR_free_strings();
-  return 0;
+    RSA_free( rsa );
+    ERR_free_strings();
+    return 0;
 }
